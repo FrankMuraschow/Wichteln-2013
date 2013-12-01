9fc7736139a24ffde38e249d3c8030476944372c
unknown <Frank@.(none)>
2013-12-01 13:42:11 +0100

    Updated mail for better readability

--- assets/mail/participationMail.php
+++ assets/mail/participationMail.php
@@ -1,4 +1,4 @@
-<body style="background: rgba(4,4,16,1);">
+<body style="background: rgba(255, 255, 255,1); color: #fff:">
 	<p style="color: #fff">Bitte setzt den Absender <b>wichteln@netznova.de</b> auf die Liste der sicheren Kontakte.</p>
 	<div style="background: rgba(47, 49, 97, 1); width: 400px; padding: 50px 100px; border: 1px solid #e5ecff;">
 		<a style="display: block; text-align: center; font-style: normal; line-height: 64px; color: #e5ecff; text-decoration: none;" href="confirmUrl">
@@ -6,4 +6,7 @@
 			<h3>Best&auml;tige deine Teilnahme beim Wichteln 2013! Damit bist du dann ganz offiziell dabei :D</h3>
 		</div> </a>
 	</div>	
+
+Solltest du die Nachricht nicht ordentlich lesen koennen, kopiere diesen Link und ruf ihn manuell auf:<br /><br />
+confirmUrl
 </body>
